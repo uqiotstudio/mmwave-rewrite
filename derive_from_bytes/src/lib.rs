d3c466fad147c97cd70b8f1142843c700adca633
mcarthur <mcarthur.alford@proton.me>
2023-12-20 16:50:38 +1000

    Adding attributes to derive_bytes

--- derive_from_bytes/src/lib.rs
+++ derive_from_bytes/src/lib.rs
@@ -2,9 +2,9 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Lit, Meta, NestedMeta};
 
-#[proc_macro_derive(FromBytes)]
+#[proc_macro_derive(FromBytes, attributes(Header, Body))]
 pub fn from_bytes_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -14,22 +14,51 @@ pub fn from_bytes_derive(input: TokenStream) -> TokenStream {
             match data_struct.fields {
                 Fields::Named(ref fields) => {
                     let mut field_init = Vec::new();
-                    let index_quote = quote! { let mut index: usize = 0; };
-
+                    let index_quote = quote! {
+                        let mut index: usize = 0;
+                    };
                     let mut iterator = fields.named.iter().peekable();
+                    let headers_quote = quote! {
+                        let mut headers = std::collections::HashMap::<usize, usize>::new();
+                    };
+
+                    // Build up the entries
                     while let Some(f) = iterator.next() {
                         let name = &f.ident;
                         let ty = &f.ty;
-                        let width = if iterator.peek().is_none() {
+                        let mut width = if iterator.peek().is_none() {
                             // The next element is none so use ALL bytes
                             quote! { index.. }
                         } else {
                             quote! { index..index+<#ty>::size_of() }
                         };
+
+                        let mut header_quote = quote! {};
+
+                        for attr in &f.attrs {
+                            if let Ok(Meta::List(meta_list)) = attr.parse_meta() {
+                                for nested_meta in meta_list.nested {
+                                    if let NestedMeta::Lit(Lit::Int(lit_int)) = nested_meta {
+                                        let value = lit_int.base10_parse::<u32>().unwrap();
+                                        if attr.path.is_ident("Header") {
+                                            header_quote = quote! {
+                                                headers.insert(value, parsed.get_body_length());
+                                            };
+                                        } else if attr.path.is_ident("Body") {
+                                            width = quote! {
+                                                index..(index+headers.get(#value).unwrap().get_body_length())
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
                         let field_quote = quote! {
                             #name: {
                                 let parsed = <#ty as FromBytes>::from_bytes(&bytes[#width]);
-                                index += <#ty>::size_of();
+                                #header_quote
+                                index += (#width.end - 1) - #width.start;
                                 parsed?
                             }
                         };
@@ -41,6 +70,7 @@ pub fn from_bytes_derive(input: TokenStream) -> TokenStream {
                             fn from_bytes(bytes: &[u8]) -> Result<Self, ParseError> {
                                 {
                                     #index_quote
+                                    #headers_quote
                                     Ok(#struct_name {
                                         #( #field_init, )*
                                     })
--- src/message.rs
+++ src/message.rs
@@ -22,6 +22,11 @@ where
     }
 }
 
+trait Header {
+    // Returns the length in bytes of the associated body (excluding the header!!)
+    fn get_body_length(&self) -> usize;
+}
+
 impl FromBytes for u8 {
     fn from_bytes(bytes: &[u8]) -> Result<Self, ParseError> {
         if bytes.len() != 1 {
@@ -86,57 +91,38 @@ impl<T: Default + Copy + FromBytes, const N: usize> FromBytes for [T; N] {
     }
 }
 
-impl<T: FromBytes> FromBytes for Vec<T> {
-    fn from_bytes(bytes: &[u8]) -> Result<Self, ParseError>
-    where
-        Self: Sized,
-    {
-        let item_size = T::size_of();
-        if bytes.len() % item_size != 0 {
-            return Err(ParseError::DataLengthMismatch);
-        }
-        let mut items = Vec::new();
-        for i in 0..(bytes.len() / item_size) {
-            let element = T::from_bytes(&bytes[i * item_size..(i + 1) * item_size])?;
-            items.push(element);
-        }
-        Ok(items)
-    }
+// impl<T: FromBytes> FromBytes for Vec<T> {
+//     fn from_bytes(bytes: &[u8]) -> Result<Self, ParseError>
+//     where
+//         Self: Sized,
+//     {
+//         let item_size = T::size_of();
+//         if bytes.len() % item_size != 0 {
+//             return Err(ParseError::DataLengthMismatch);
+//         }
+//         let mut items = Vec::new();
+//         for i in 0..(bytes.len() / item_size) {
+//             let element = T::from_bytes(&bytes[i * item_size..(i + 1) * item_size])?;
+//             items.push(element);
+//         }
+//         Ok(items)
+//     }
 
-    fn size_of_val(&self) -> usize {
-        // Get the size of all vals and sum them, instead of returning the standard vec size!
-        self.iter().map(|t| t.size_of_val()).sum()
-    }
-}
+//     fn size_of_val(&self) -> usize {
+//         // Get the size of all vals and sum them, instead of returning the standard vec size!
+//         self.iter().map(|t| t.size_of_val()).sum()
+//     }
+// }
+// TODO change this to parse T blindly (providing &bytes[offset..]), then use T::size_of_val() to find where the slice ends in retrospect and move offset up to that
+// This then relies on the size of function being reliable!!!!
+// Then we simply go until we hit offset >= bytes.len(), and return after that.
 
+#[derive(FromBytes)]
 struct Frame {
+    #[Header(1)]
     frame_header: FrameHeader,
+    #[Body(1)]
     frame_body: FrameBody,
-}
-
-impl FromBytes for Frame {
-    fn from_bytes(bytes: &[u8]) -> Result<Self, ParseError>
-    where
-        Self: Sized,
-    {
-        let mut offset = 0;
-        let frame_header =
-            FrameHeader::from_bytes(&bytes[offset..offset + FrameHeader::size_of()])?;
-
-        offset += FrameHeader::size_of();
-
-        let frame_body =
-            FrameBody::from_bytes(&bytes[offset..frame_header.packet_length as usize])?;
-
-        if frame_body.tlvs.len() != frame_header.num_tlvs as usize {
-            Err(ParseError::DataLengthMismatch)
-        } else {
-            Ok(Frame {
-                frame_header,
-                frame_body,
-            })
-        }
-    }
 }
 
 #[derive(FromBytes)]
@@ -152,57 +138,20 @@ struct FrameHeader {
     subframe_num: u32,
 }
 
+#[derive(FromBytes)]
 struct FrameBody {
     tlvs: Vec<Tlv>,
 }
 
-impl FromBytes for FrameBody {
-    fn from_bytes(bytes: &[u8]) -> Result<Self, ParseError>
-    where
-        Self: Sized,
-    {
-        let mut tlvs = Vec::new();
-        let mut offset = 0;
-
-        while offset + TlvHeader::size_of() < bytes.len() {
-            let tlv_header = TlvHeader::from_bytes(&bytes[offset..offset + TlvHeader::size_of()])?;
-
-            offset += TlvHeader::size_of();
-
-            // Slice the bytes to only the length of the tlv remaining!
-            let bytes = &bytes[offset..offset + tlv_header.length as usize];
-
-            let tlv_body = match tlv_header.tlv_type {
-                TlvType::PointCloud => TlvBody::PointCloud(Vec::from_bytes(&bytes)?),
-                TlvType::RangeProfile => TlvBody::RangeProfile(Vec::from_bytes(&bytes)?),
-                TlvType::NoiseProfile => TlvBody::NoiseProfile(Vec::from_bytes(&bytes)?),
-                TlvType::StaticAzimuthHeatmap => {
-                    TlvBody::StatisticAzimuthHeatmap(Vec::from_bytes(&bytes)?)
-                }
-                TlvType::RangeDopplerHeatmap => TlvBody::RangeDopplerHeatmap,
-                TlvType::Statistics => TlvBody::Statistics,
-                TlvType::SideInfo => TlvBody::SideInfo(Vec::from_bytes(&bytes)?),
-                TlvType::AzimuthElevationStaticHeatmap => {
-                    TlvBody::AzimuthElevationStaticHeatmap(Vec::from_bytes(&bytes)?)
-                }
-                TlvType::Temperature => TlvBody::Temperature,
-            };
-
-            tlvs.push(Tlv {
-                tlv_header,
-                tlv_body,
-            })
-        }
-
-        Ok(FrameBody { tlvs })
-    }
-}
-
+#[derive(FromBytes)]
 struct Tlv {
+    #[Header(1)]
     tlv_header: TlvHeader,
+    #[Body(1)]
     tlv_body: TlvBody,
 }
 
+#[derive(FromBytes)]
 enum TlvBody {
     PointCloud(Vec<[f32; 4]>),
     RangeProfile(Vec<[u8; 2]>),
