6b20a4cc15544d5a65e3f0babc188fc7b8eb0716
mcarthur <mcarthur.alford@proton.me>
2024-02-01 11:11:30 +1000

    default for config

--- client/src/main.rs
+++ client/src/main.rs
@@ -19,7 +19,12 @@ async fn main() {
     let args: Vec<String> = env::args().collect();
 
     let ip_address = args.get(1).cloned().unwrap_or("localhost".to_owned());
-    let machine_id: usize = args.get(2).cloned().unwrap_or("0".to_owned()).parse().expect("Requires positive number for machine_id");
+    let machine_id: usize = args
+        .get(2)
+        .cloned()
+        .unwrap_or("0".to_owned())
+        .parse()
+        .expect("Requires positive number for machine_id");
 
     let url = loop {
         match Url::parse(&format!("ws://{}:3000/ws", ip_address)) {
