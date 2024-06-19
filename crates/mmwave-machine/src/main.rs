89e2b70c04b6187746fa6bfb5bd0a6603369fb80
mcarthur <mcarthur.alford@proton.me>
2024-06-19 02:44:50 +0000

    machine reconnects properly

--- crates/mmwave-machine/src/main.rs
+++ crates/mmwave-machine/src/main.rs
@@ -2,13 +2,11 @@ mod args;
 
 use args::Args;
 use async_nats::jetstream;
-
 use clap::Parser;
 use futures::StreamExt;
 use mmwave_core::{address::ServerAddress, config::Configuration, logging::enable_tracing};
-use std::error::Error;
-
-use tracing::info;
+use std::{error::Error, time::Duration};
+use tracing::{error, info};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -17,27 +15,55 @@ async fn main() -> Result<(), Box<dyn Error>> {
         enable_tracing(args.debug, args.log_relay);
     }
 
-    let address = ServerAddress::new(args.ip, args.port).await;
+    let mut address = ServerAddress::new(args.ip, args.port).await;
 
+    loop {
+        if let Err(e) = handle_nats(address.clone()).await {
+            error!(error=%e, "nats exited with error")
+        }
+        info!("Attempting to re-establish connection");
+        tokio::time::sleep(Duration::from_millis(1000)).await;
+        address.refresh().await;
+    }
+}
+
+async fn handle_nats(address: ServerAddress) -> Result<(), Box<dyn Error>> {
     // Connect to the NATS server
     let client = async_nats::connect(address.address().to_string()).await?;
     let jetstream = jetstream::new(client);
 
-    let configs = jetstream
-        .create_key_value(jetstream::kv::Config {
-            bucket: "config".to_string(),
-            history: 10,
-            ..Default::default()
-        })
-        .await?;
+    let configs = match jetstream.get_key_value("config").await {
+        Ok(configs) => configs,
+        Err(_) => {
+            info!("config bucket does not exist, creating a default config");
+            jetstream
+                .create_key_value(jetstream::kv::Config {
+                    bucket: "config".to_string(),
+                    history: 10,
+                    ..Default::default()
+                })
+                .await?
+        }
+    };
 
     let default_config = Configuration::default();
     let serialized = serde_json::to_string(&default_config)?;
-    configs.put("config", serialized.into()).await?;
+    configs.put("config", serialized.clone().into()).await?;
+
+    info!(config = serialized, "put config into kv store");
 
     let mut entries = configs.watch("config").await?;
+    info!("Watching for config updates");
     while let Some(config) = entries.next().await {
-        info!(config=?config, "Found updated config");
+        match config {
+            Err(e) => {
+                error!(error=%e, "something went wrong watching for config updates");
+                break;
+            }
+            Ok(config) => {
+                info!(config=?config, "Found updated config");
+            }
+        }
     }
 
     Ok(())
---
+++ nats_server.sh
@@ -0,0 +1,2 @@
+rm -r ./js_store
+nats-server --port 3000 -js -sd ./js_store
